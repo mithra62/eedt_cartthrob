53a8e11eb275f0bbb6ea20e700545bbb5001cc67
Eric Lamb <eric@mithra62.com>
2024-01-21 09:25:10 -0700

    adds check to ensure CartThrob is installed before trying to compile data

--- system/user/addons/eedt_cartthrob/Actions/GetCtDebug.php
+++ system/user/addons/eedt_cartthrob/Actions/GetCtDebug.php
@@ -2,14 +2,24 @@
 
 namespace DebugToolbar\CartThrob\Actions;
 
-use ExpressionEngine\Service\Addon\Controllers\Action\AbstractRoute;
+use DebugToolbar\Actions\AbstractAction;
 
-class GetCtDebug extends AbstractRoute
+class GetCtDebug extends AbstractAction
 {
-    public function process()
+    public function processDebug()
     {
-        $panel = ee('eedt_cartthrob:PanelService');
-        echo ee()->load->view('eedt_cartthrob', $panel->compilePanelVars(), true);
-        exit;
+        if($this->toolbar->isAddonInstalled('cartthrob')) {
+
+            loadCartThrobPath();
+            if (!ee('ee_debug_toolbar:ToolbarService')->canViewToolbar()) {
+                return;
+            }
+
+            $panel = ee('eedt_cartthrob:PanelService');
+            echo ee()->load->view('eedt_cartthrob', $panel->compilePanelVars(), true);
+            exit;
+        } else {
+            echo "CartThrob isn't installed!";
+        }
     }
 }
